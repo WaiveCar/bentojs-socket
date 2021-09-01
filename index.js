898732b151dc4faf3bde960be9d1c2bdbfeb3101
Chris McKenzie <kristopolous@yahoo.com>
2021-09-01 11:16:54 -0700

    socket updates

--- index.js
+++ index.js
@@ -7,30 +7,22 @@ let Client = require('./lib/services/client');
 module.exports = function load(config) {
   auth.setup(config);
 
-  // ### Setup Adapter
 
   if (config.redis) {
-    let redis = require('socket.io-redis');
+    let redis = require('@socket.io/redis-adapter');
     io.adapter(redis({
       host : config.redis.host,
       port : config.redis.port
     }));
   }
 
-  // ### Origins
-  // Set the allowed access origins.
-
   if (config.origins) {
     io.set('origins', (Object.prototype.toString.call(config.origins) === '[object Array]' ? config.origins.join(',') : config.origins));
   }
 
-  // ### Connect
-
   io.on('connection', (socket) => {
     new Client(io, socket);
   });
-
-  // ### Listen
 
   io.listen(config.port);
   console.log(`Server listening on port ${ config.port }`);
